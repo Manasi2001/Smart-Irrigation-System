f370dc73987e8642f10405964b8bd1396e2b7f88
Manasi Chhibber <68627617+Manasi2001@users.noreply.github.com>
2021-06-20 10:41:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import joblib
 classifier = joblib.load(r'smartirrigation.pkl')
 
@@ -26,5 +27,5 @@ def green_hacks():
     return render_template('index.html')
   
 if __name__=='__main__':
-   app.run(debug=True)
-   const port = process.env.port || 5000
+   port = int(os.environ.get("PORT", 5000))
+   app.run(host='0.0.0.0', port=port)
